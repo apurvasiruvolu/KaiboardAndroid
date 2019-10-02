7f61b7c69573e7101adcbc325e36f64b968a4471
apurvasiruvolu <apurva.complit@complitsolutions.onmicrosoft.com>
2019-10-02 12:33:29 +0530

    Initial check-in of module KaiboardAndroid

---
+++ KaiboardAndroid/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Android.App;
+using Android.InputMethodServices;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace KaiboardAndroid
+{
+    [Service(Permission = "android.permission.BIND_INPUT_METHOD", Label = "Kaiboard")]
+    [MetaData("android.view.im", Resource = "@xml/method")]
+    [IntentFilter(new string[] { "android.view.InputMethod" })]
+    public class MainActivity : InputMethodService, KeyboardView.IOnKeyboardActionListener
+    {
+        private KeyboardView kv;
+        private Keyboard keyboard;
+        //private bool isCaps = false;
+
+        public override View OnCreateInputView()
+        {
+            kv = (KeyboardView)LayoutInflater.Inflate(Resource.Layout.Keyboard, null);
+            keyboard = new Keyboard(this, Resource.Xml.qwerty);
+            kv.Keyboard = keyboard;
+            kv.OnKeyboardActionListener = this;
+            return kv;
+        }
+
+        public void OnKey([GeneratedEnum] Android.Views.Keycode primaryCode, [GeneratedEnum] Android.Views.Keycode[] keyCodes)
+        {
+            
+        }
+
+        public void OnPress([GeneratedEnum] Android.Views.Keycode primaryCode)
+        {
+            
+        }
+
+        public void OnRelease([GeneratedEnum] Android.Views.Keycode primaryCode)
+        {
+            
+        }
+
+        public void OnText(ICharSequence text)
+        {
+            
+        }
+
+        public void SwipeDown()
+        {
+            
+        }
+
+        public void SwipeLeft()
+        {
+            
+        }
+
+        public void SwipeRight()
+        {
+            
+        }
+
+        public void SwipeUp()
+        {
+           
+        }
+    }
+}
+
